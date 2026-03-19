86fd55934bbb217f42dcd67717db99ac1d8a3967
joel-30551 <jasante137@st.ug.edu.gh>
2026-03-19 07:32:31 +0000

    initial commit

--- Question_2/testStudent.php
+++ Question_2/testStudent.php
@@ -6,7 +6,7 @@ echo"Name:". $student1->getName(). "<br>";
 echo"Student ID:". $student1->getStudentId(). "<br>";
 echo"<p>";
 
-echo"Score Between 0 and 100: (78.5) <br> ";
+echo"Score Between 0 and 100: (58.5) <br> ";
 echo"Grade:". $student1->getGrade() ."<br>";
 echo"<p>";
 
